26398bded18f87f2cedcb3e937c15e085e55f15f
Lifguson <lwellsteadgi@gmail.com>
2022-12-20 12:07:38 +0100

    one more button animation

--- src/EditTask.js
+++ src/EditTask.js
@@ -1,59 +1,69 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState, useEffect } from "react";
 
-const EditTask = ({ editedTask, updateTask, closeEditMode, error, showModal, setShowModal }) => {
-    const [updatedTaskName, setUpdatedTaskName] = useState(editedTask.text);
+const EditTask = ({
+  editedTask,
+  updateTask,
+  closeEditMode,
+  error,
+  showModal,
+  setShowModal,
+}) => {
+  const [updatedTaskName, setUpdatedTaskName] = useState(editedTask.text);
 
-    useEffect(() => {
-        const closeModalIfEscaped = (e) => {
-            e.key === "Escape" && closeEditMode();
-        }
-
-        window.addEventListener("keydown", closeModalIfEscaped)
-
-        return () => {
-            window.removeEventListener("keydown", closeModalIfEscaped)
-        }
-    }, [closeEditMode])
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        updateTask({...editedTask, text: updatedTaskName})
-    }
-
-    const backdropVariants = {
-      initial: { opacity: 0 },
-      animate: { opacity: 1 },
+  useEffect(() => {
+    const closeModalIfEscaped = (e) => {
+      e.key === "Escape" && closeEditMode();
     };
-    
-    const modalVariants = {
-        initial: {
-            y: "-100vh",
-            opacity: 0,
-        },
-        animate: {
-            y: "200px",
-            opacity: 1,
-            transition: { delay: 0.2 }
-        }
-    }
 
-    return (
+    window.addEventListener("keydown", closeModalIfEscaped);
+
+    return () => {
+      window.removeEventListener("keydown", closeModalIfEscaped);
+    };
+  }, [closeEditMode]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateTask({ ...editedTask, text: updatedTaskName });
+  };
+
+  const backdropVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
+  const modalVariants = {
+    initial: {
+      y: "-100vh",
+      opacity: 0,
+    },
+    animate: {
+      y: "200px",
+      opacity: 1,
+      transition: { delay: 0.2 },
+    },
+  };
+
+  return (
     <AnimatePresence>
-      { showModal && (
+      {showModal && (
         <motion.div
-        className="backdrop"
-        variants={backdropVariants}
-        initial="initial"
-        animate="animate"
-        exit="initial"
-      >
-        <motion.div className="edit-input"
+          className="backdrop"
+          variants={backdropVariants}
+          initial="initial"
+          animate="animate"
+          exit="initial"
+        >
+          <motion.div
+            className="edit-input"
             role="dialog"
             aria-labelledby="editTask"
-            onClick={(e) => {e.target === e.currentTarget && closeEditMode()}}
+            onClick={(e) => {
+              e.target === e.currentTarget && closeEditMode();
+            }}
             variants={modalVariants}
-        >
+          >
             <form onSubmit={handleSubmit}>
               <input
                 type="text"
@@ -64,12 +74,15 @@ const EditTask = ({ editedTask, updateTask, closeEditMode, error, showModal, set
                 required
                 autoFocus
               />
-              <button className="update-button"
+              <motion.button
+                className="update-button"
                 type="submit"
                 aria-label={`confirm edited task to now read ${updatedTaskName}`}
+                style={{ backgroundColor: "transparent" }}
+                whileHover={{ scale: 1.1 }}
               >
                 Update
-              </button>
+              </motion.button>
               <p
                 style={{
                   display: !error ? "none" : "",
@@ -82,9 +95,8 @@ const EditTask = ({ editedTask, updateTask, closeEditMode, error, showModal, set
           </motion.div>
         </motion.div>
       )}
-    
-      </AnimatePresence>
-    )
-}
+    </AnimatePresence>
+  );
+};
 
 export default EditTask;
